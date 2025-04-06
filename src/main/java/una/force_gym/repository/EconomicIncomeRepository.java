21af8b9224836db75ecb00fc97c6d369cbdf234b
Gerald Castillo <134823588+Gera10CC@users.noreply.github.com>
2025-04-06 17:50:14 -0600

    Se agreo en el dashboard y en el aprtado de balance economico los datos de todos los ingresos y gastos, ademas de los filtros propios de gastos e ingresos. Tambien se trabajo en los permisos de balance y gastos (#38)

--- src/main/java/una/force_gym/repository/EconomicIncomeRepository.java
+++ src/main/java/una/force_gym/repository/EconomicIncomeRepository.java
@@ -1,9 +1,6 @@
 package una.force_gym.repository;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.Date;
-import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.query.Procedure;
@@ -12,28 +9,6 @@ import org.springframework.data.repository.query.Param;
 import una.force_gym.domain.EconomicIncome;
 
 public interface EconomicIncomeRepository extends JpaRepository<EconomicIncome, Long>{
-    
-    @Procedure(procedureName = "prGetAllEconomicIncomes",outputParameterName = "result")
-    List<EconomicIncome> getAllEconomicIncomes(
-        @Param("p_filterByStatus") String filterByStatus,
-        @Param("p_filterByAmountRangeMin") BigDecimal filterByAmountRangeMin,
-        @Param("p_filterByAmountRangeMax") BigDecimal filterByAmountRangeMax,
-        @Param("p_filterByDateRangeStart") Date filterByDateRangeStart,
-        @Param("p_filterByDateRangeEnd") Date filterByDateRangeEnd,
-        @Param("p_filterByMeanOfPayment") Integer filterByMeanOfPayment,
-        @Param("p_filterByTypeClient") Long filterByTypeClient
-    );
-
-    @Procedure(procedureName = "prGetAllEconomicIncomes",outputParameterName = "result")
-    List<EconomicIncome> getAllEconomicIncomes(
-        @Param("p_filterByStatus") String filterByStatus,
-        @Param("p_filterByAmountRangeMin") BigDecimal filterByAmountRangeMin,
-        @Param("p_filterByAmountRangeMax") BigDecimal filterByAmountRangeMax,
-        @Param("p_filterByDateRangeStart") Date filterByDateRangeStart,
-        @Param("p_filterByDateRangeEnd") Date filterByDateRangeEnd,
-        @Param("p_filterByMeanOfPayment") Integer filterByMeanOfPayment,
-        @Param("p_filterByTypeClient") Long filterByTypeClient
-    );
     
     @Procedure(procedureName = "prInsertEconomicIncome", outputParameterName = "result")
     int addEconomicIncome(@Param("pIdClient") Long pIdClient, @Param("pRegistrationDate") LocalDate pRegistrationDate, @Param("pVoucherNumber") String pVoucherNumber, @Param("pDetail") String pDetail, @Param("pIdMeanOfPayment") Long pIdMeanOfPayment, @Param("pAmount") Float pAmount, @Param("pIdActivityType") Long pIdActivityType, @Param("pLoggedIdUser") Long pLoggedIdUser);
