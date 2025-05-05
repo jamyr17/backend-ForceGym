1af88be291e46face8bf673a5189b0ec67d2184b
Jamyr González <135465554+jamyr17@users.noreply.github.com>
2025-05-04 21:44:54 -0600

    [04/05 14:37h] Se implementó el resto de funcionalidades CRUD para categorías de ejercicios (#69)

--- src/main/java/una/force_gym/repository/ExerciseCategoryRepository.java
+++ src/main/java/una/force_gym/repository/ExerciseCategoryRepository.java
@@ -4,12 +4,33 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
 
 import una.force_gym.domain.ExerciseCategory;
 
 public interface ExerciseCategoryRepository extends JpaRepository<ExerciseCategory, Long> {
 
-    @Procedure(procedureName = "prGetExerciseCategory")
+    @Procedure(procedureName = "prGetAllExerciseCategory")
     List<ExerciseCategory> getExercisesCategories();
+
+    @Procedure(procedureName = "prInsertExerciseCategory", outputParameterName = "result")
+    int addExerciseCategory(
+        @Param("pName") String name,
+        @Param("pLoggedIdUser") Long loggedIdUser
+    );
+
+    @Procedure(procedureName = "prUpdateExerciseCategory", outputParameterName = "result")
+    int updateExerciseCategory(
+        @Param("pIdExerciseCategory") Long idExerciseCategory,
+        @Param("pName") String name,
+        @Param("pIsDeleted") Long pIsDeleted,  
+        @Param("pLoggedIdUser") Long loggedIdUser
+    );
+
+    @Procedure(procedureName = "prDeleteExerciseCategory", outputParameterName = "result")
+    int deleteExerciseCategory(
+        @Param("pIdExerciseCategory") Long idExerciseCategory,
+        @Param("pLoggedIdUser") Long loggedIdUser
+    );
 
 }
