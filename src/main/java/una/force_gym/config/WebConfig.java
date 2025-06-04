0e12de828fadedd64ff6cc8cdede7cf7f9981344
Jamyr González <josuegonza1702@gmail.com>
2025-06-04 15:08:26 -0600

    Adding localhost domain

--- src/main/java/una/force_gym/config/WebConfig.java
+++ src/main/java/una/force_gym/config/WebConfig.java
@@ -25,6 +25,7 @@ public class WebConfig implements WebMvcConfigurer {
         config.setAllowCredentials(true);
         config.addAllowedOrigin("https://frontend-force-gym.vercel.app");
         config.addAllowedOrigin("https://frontend-force-gym-git-staging-jamyr-gonzalezs-projects.vercel.app");
+        config.addAllowedOrigin("https://localhost");
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
