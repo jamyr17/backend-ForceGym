e083865516260e3d813aa294882a6dfb6de395f5
Jamyr González <josuegonza1702@gmail.com>
2025-06-04 13:04:20 -0600

    Work hibernate bean with Hibernate 6

--- src/main/java/una/force_gym/config/HibernateModuleConfig.java
+++ src/main/java/una/force_gym/config/HibernateModuleConfig.java
@@ -1,24 +1,18 @@
-package main.java.una.force_gym.config;
+package una.force_gym.config;
 
 import com.fasterxml.jackson.databind.Module;
-import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilderCustomizer;
 
 @Configuration
 public class HibernateModuleConfig {
 
     @Bean
-    public Jackson2ObjectMapperBuilderCustomizer jacksonCustomizer() {
-        return builder -> {
-            Hibernate5Module hibernate5Module = new Hibernate5Module();
-            // Ignorar las proxies no inicializadas para evitar excepciones
-            hibernate5Module.disable(Hibernate5Module.Feature.USE_TRANSIENT_ANNOTATION);
-            // También es común deshabilitar FORCE_LAZY_LOADING si no quieres que se cargue toda la info automáticamente
-            hibernate5Module.disable(Hibernate5Module.Feature.FORCE_LAZY_LOADING);
-            
-            builder.modules(hibernate5Module);
-        };
+    public Module hibernate6Module() {
+        Hibernate6Module module = new Hibernate6Module();
+        module.disable(Hibernate6Module.Feature.FORCE_LAZY_LOADING);
+        module.disable(Hibernate6Module.Feature.USE_TRANSIENT_ANNOTATION);
+        return module;
     }
 }
